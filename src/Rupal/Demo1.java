d47521a4162714703f70ab81c4aa9b94b503ed77
sakshi <shendesakshi25@gmail.com>
2025-02-09 15:50:52 +0530

    implementation by sakshi

--- src/Rupal/Demo1.java
+++ src/Rupal/Demo1.java
@@ -3,12 +3,13 @@ package Rupal;
 public class Demo1 {
 
 	public static void main(String[] args) {
-		String str="Sakshi";
+		String str="Sahil";
 		for(int i=str.length()-1;i>=0;i--)
 		{
 			System.out.println(str.charAt(i));
 		}
-
+		System.out.println("test 1");
+		System.out.println("test 2");
 	}
 
 	
